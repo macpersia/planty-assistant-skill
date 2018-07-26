40f1205f58c06e562c91313635303a6c93ae4a97
macpersia <macpersia@gmail.com>
2018-07-26 20:31:23 +0800

    Updated a test unit

---
+++ src/test/java/be/planty/skills/prototyping/handlers/agent/AgentClientTest.java
@@ -0,0 +1,38 @@
+package be.planty.skills.prototyping.handlers.agent;
+
+import be.planty.skills.assistant.handlers.agent.AgentClient;
+import com.amazon.ask.model.Response;
+import org.apache.http.auth.AuthenticationException;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for the {@link AgentClient}.
+ */
+//@RunWith(SpringRunner.class)
+public class AgentClientTest {
+
+    static final AgentClient agentClient = new AgentClient();
+
+    @Test
+    public void messageAgent() throws ExecutionException, InterruptedException, TimeoutException, AuthenticationException {
+        final CompletableFuture<Optional<Response>> futureSession = agentClient.messageAgent(null);
+        assertNotNull(futureSession);
+        final Optional<Response> optResponse = futureSession.get(5, SECONDS);
+        assertTrue("No response is present!", optResponse.isPresent());
+        assertEquals("Some speech!", optResponse.get().getOutputSpeech());
+    }
+
+    @AfterClass
+    public static void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+}
